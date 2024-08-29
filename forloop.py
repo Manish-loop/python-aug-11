91bf76ef592eddfc51ef94e6c64e0113937d758c
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2024-08-29 21:47:17 +0545

    Practice commit

---
+++ forloop.py
@@ -0,0 +1,9 @@
+# for item in list_of items:
+#  Do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "Pie")
+    print(fruits)   # Here print statement is inside for loop so 3 times print 
+print(fruits)  # Here print statement is outside for loop so 1 times print
